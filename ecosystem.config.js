73386a55ba67da63ae42075a6f045360597c2ad4
Konstantin podlesny <konstantin-921@yandex.ru>
2019-04-26 15:36:33 +0300

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'AmazonEc2-Nodejs-Angular-example',
+    script: 'server/.server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-3-83-25-230.compute-1.amazonaws.com',
+      key: '~/kostya-key-Virginia.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:konstantin-921/AmazonEc2-Nodejs-Angular-example.git',
+      path: '/home/ec2-user/AmazonEc2-Nodejs-Angular-example',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
