277d5d53aef760f0492b71c4a640a5cca35f3097
Konstantin podlesny <konstantin-921@yandex.ru>
2019-04-29 13:45:32 +0300

    Add first request

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+fetch('http://localhost:3000').then(function(response) {
+  console.log(response);
+});
+
 if (environment.production) {
   enableProdMode();
 }
