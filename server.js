67add8ea203b48c943db7f8ed05d454646fe4d53
Konstantin podlesny <konstantin-921@yandex.ru>
2019-04-29 10:04:02 +0300

    Change publick path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('HEY!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
