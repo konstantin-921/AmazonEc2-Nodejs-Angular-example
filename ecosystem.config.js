2293f541cf442bcbf4503b33e2f1ef5c2ea17b3e
Konstantin podlesny <konstantin-921@yandex.ru>
2019-04-29 09:05:23 +0300

    Change host 29.04.2019

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ec2-user',
-      host: 'ec2-3-83-25-230.compute-1.amazonaws.com',
+      host: 'ec2-3-95-158-214.compute-1.amazonaws.com',
       key: '~/kostya-key-Virginia.pem',
       ref: 'origin/master',
       repo: 'git@github.com:konstantin-921/AmazonEc2-Nodejs-Angular-example.git',
